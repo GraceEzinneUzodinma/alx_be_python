a0e0fcb084b6baa0a89cb86f1189def35b28f056
grace uzodinma <guzodinma2@gmail.com>
2025-10-04 10:59:05 +0100

    account balance

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+        self.initial_balance =  0
+    def deposit(self, amount):
+        self.amount = amount
+        if amount < initial_balance:
+            return "invalid amount"
+        else:
+            return account_balance + amount
+    def withdraw(self, amount):
+        if amount < account_balance:
+            return account_balance - amount
+        else:
+            return "insufficient balance"
+    def display_balance(self, amount):
+        print(f"current balance: ${amount}")
