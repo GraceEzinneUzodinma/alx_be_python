86f14bbfb302acb1199cb8e6d408e7c9f1a37f01
grace uzodinma <guzodinma2@gmail.com>
2025-10-04 11:40:46 +0100

    bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -11,6 +11,6 @@ class BankAccount:
         if amount < self.account_balance:
             return self.account_balance - amount
         else:
-            return "insufficient balance"
+            return "insufficient funds."
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
