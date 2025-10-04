d33e4ea8548187ea05e13cfa54d82b2b70900702
grace uzodinma <guzodinma2@gmail.com>
2025-10-04 17:21:09 +0100

    library_management

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,25 @@
+class Book:
+    def __init__(self, title, author):
+        self.title =  title
+        self.author = author
+        self.__is_checked_out = False
+
+class Library:
+    def __init__(self):
+        self.__books = []
+    def add_book(self, book):
+        self.__books.append(book)
+    
+    def list_available_books(self):
+        for book in self.__books:
+            return (book)
+    def check_out_book(self, title):
+        for book in self.__books:
+            if book.title == title and not book.__is_checked_out:
+                book.__is_checked_out = True
+                return
+    def return_book(self, title):
+        for book in self.__books:
+            if book.title == title and book.__is_checked_out:
+                book.__is_checked_out = False
+                return
