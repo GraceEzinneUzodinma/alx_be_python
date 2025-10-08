bdf0f7613024c47dc8b5af1950203cd864501b0b
grace uzodinma <guzodinma2@gmail.com>
2025-10-08 12:53:20 +0100

    library

---
+++ oop/library_system.py
@@ -0,0 +1,20 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+class Ebook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = int(file_size)
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count =int(page_count)
+class Library:
+    def __init__(self):
+        self.books = []
+    def add_book(self, book):
+        self.books.append(book)
+    def list_books(self):
+        for book in self.books:
+            print(f"{self.title} by {self.author}")
